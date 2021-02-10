76dcc0bd9103dcb917ca2866cdf987ddf7addcc3
Jacek <ja_cek@gmx.com>
2021-02-10 16:32:04 +0100

    app.js update

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -12,7 +12,7 @@ class ShoppingCart {
 
     addProduct(product) {
         this.items.push(product);
-        this.totalOutput = `<h2>Total: \$${1}</h2>`;
+        this.totalOutput.innerHTML = `<h2>Total: \$${1}</h2>`;
     }
 
     render() {
@@ -33,8 +33,7 @@ class ProductItem {
     }
 
     addToCart() {
-        console.log('Adding product to cart...');
-        console.log(this.product);
+        App.addProductToCart(this.product);
     }
 
     render() {
@@ -93,8 +92,8 @@ class Shop {
     render() {
         const renderHook = document.getElementById('app');
         
-        const cart = new ShoppingCart();
-        const cartEl = cart.render();
+        this.cart = new ShoppingCart();
+        const cartEl = this.cart.render();
         const productList = new ProductList();
         const prodListEl = productList.render();
 
@@ -103,5 +102,17 @@ class Shop {
     }
 }
 
-    const shop = new Shop();
-    shop.render();
+class App {
+    static cart;
+
+    static init() {
+        const shop = new Shop();
+        shop.render()
+        this.cart = shop.cart;
+    }
+    static addProductToCart(product) {
+        this.cart.addProduct(product);
+    }
+}
+
+App.init();
